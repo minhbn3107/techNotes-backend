f1887e6916a8998312dbfd08c4dade65f0a4ede3
minhbn3107 <109571044+minhbn3107@users.noreply.github.com>
2023-11-20 20:34:10 +0700

    fix link

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
     "http://localhost:3000",
-    "https://technotes-iby1.onrender.com/",
+    "https://technotes-iby1.onrender.com",
 ];
 
 module.exports = allowedOrigins;
