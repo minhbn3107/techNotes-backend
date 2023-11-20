b263982f9b5cfdf3aa82cae613d586579d8bc676
minhbn3107 <109571044+minhbn3107@users.noreply.github.com>
2023-11-20 17:59:09 +0700

    update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,6 @@
 const allowedOrigins = [
     "http://localhost:3000",
-    "https://www.dandrepairshop.com",
-    "https:dandrepairshop.com",
+    "https://technotes-iby1.onrender.com/",
 ];
 
 module.exports = allowedOrigins;
